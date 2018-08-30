3d3811ff109e5918230e75c09608a819b27486c8
mmmaaaggg <mmmaaaggg@163.com>
2018-08-30 14:23:41 +0800

    return None when -4001 error

--- direstinvoker/iwind.py
+++ direstinvoker/iwind.py
@@ -32,7 +32,11 @@ class WindRestInvoker:
             ret_dic = None
 
         if ret_data.status_code != 200:
-            raise APIError(ret_data.status_code, ret_dic)
+            if ret_dic is None and 'error_code' in ret_dic and ret_dic['error_code'] == -4001:
+                logger.error('%s post %s got error\n%s', self._url(path), req_data, ret_dic)
+                return None
+            else:
+                raise APIError(ret_data.status_code, ret_dic)
         else:
             return ret_dic
 
